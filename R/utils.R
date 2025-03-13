dd5381af9ec6c56ab67816395c68c4a099695551
Val. Lucet <VLucet@users.noreply.github.com>
2025-03-13 11:04:27 -0400

    Increase timeouts

--- R/utils.R
+++ R/utils.R
@@ -155,7 +155,7 @@
     req_headers(Authorization = paste("Bearer", ._wt_auth_env_$access_token)) |>
     req_user_agent(u) |>
     req_method("POST") |>
-    req_timeout(3000) |> # Add a 5 minute timeout
+    req_timeout(1000000) |> # Blow this up
     req_perform()
 
   # Handle errors
